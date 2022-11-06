5978b04715ed969698e697f50e41eb792601be87
GDBmy <113766976+GDBmy@users.noreply.github.com>
2022-11-06 14:55:20 +0800

    Update GithubTest.java
    github_test3

--- src/main/java/GithubTest.java
+++ src/main/java/GithubTest.java
@@ -2,5 +2,6 @@ public class GithubTest {
     public static void main(String [] args){
         System.out.println("This is git one");
         System.out.println("This is git two");
+        System.out.println("This is git three");
     }
 }
